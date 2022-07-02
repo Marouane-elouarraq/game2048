4bc11c789772c67674dfa1b1881b8398841da89e
Sakuragi-crypto <108550029+Sakuragi-crypto@users.noreply.github.com>
2022-07-02 21:22:07 +0200

    no message

--- test_grid_2048.py
+++ test_grid_2048.py
@@ -1,7 +1,9 @@
-from game2048-repo.grid_2048 import create_grid
+from grid_2048 import create_grid
 from pytest import *
 
 
 def test_create_grid():
     assert create_grid() == [[' ', ' ', ' ', ' '], [' ', ' ', ' ', ' '], [
         ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ']]
+
+test_create_grid()
