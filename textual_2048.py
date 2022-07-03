a28d9f6eeac12fb5c627afea3024ff6bc9bdb5d1
Sakuragi-crypto <108550029+Sakuragi-crypto@users.noreply.github.com>
2022-07-03 19:32:28 +0200

    textual module

---
+++ textual_2048.py
@@ -0,0 +1,6 @@
+def read_player_command():
+    move = input("Entrez votre commande (g (gauche), d (droite), h (haut), b (bas)):")
+    return move
+def red_size_grid():
+    size = input("choisissez une taille pour la grille:")
+    return size
